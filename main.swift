862475a2bec9c361932e9b41f5d851661b0e64b2
leduccuong <leduccuongwru@gmail.com>
2023-04-21 19:07:22 +0700

    baitapvenha

---
+++ main.swift
@@ -0,0 +1,75 @@
+//
+//  main.swift
+//  test3
+//
+//  Created by Cường Lê Đức on 20/04/2023.
+//
+
+//import Foundation
+
+//
+//  main.swift
+//  Homework2
+//
+//  Created by Cường Lê Đức on 20/04/2023.
+//
+
+import Foundation
+
+//baitap1
+//var mangdulieu = [1,2,-3,5,6,7,-10]
+//for  i in 0..<mangdulieu.count {
+//    if mangdulieu[i] < 0 {
+//        mangdulieu[i] = 0
+//    }
+//    print(mangdulieu)
+//}
+//// baitap2
+//var chuoi = "Hello"
+//var chuoi1 = String(chuoi.reversed())
+//print(chuoi1)
+//// bảitap3
+//let dayso = [2,3,4,5,6,7,8]
+//let soluong = dayso.count
+//let tongdayso = dayso.reduce(0, +)
+//let tbc = tongdayso / soluong
+//print("Trung bình cộng của dãy số là \(tbc)")
+////baitap4
+////var mangsonguyen = [1,2,3,4,2,4,6,7,23,123,645,213]
+////var somin = mangsonguyen[0]
+////var somax = mangsonguyen[0]
+////for so in mangsonguyen {
+////    if so < somin {
+////        somin = so
+////    }
+////    if so > somax
+////    {
+////        somax = so
+////    }
+////}
+////print(" số bé nhất là \(somin)")
+////print(" số lớn nhất là \(somax)")
+//////baitap5
+//var mangsonguyen = [1,2,3,4,2,4,6,7,23,123,645,213]
+////var somin = mangsonguyen[0]
+//var somax = mangsonguyen[0]
+//var solonthu2 = mangsonguyen?
+//for so in mangsonguyen {
+//    if so < solonthu2 && solonthu2 < somax
+//    {
+//        solonthu2 = so
+//    }
+//}
+//print(" số lớn thu 2 là \(solonthu2)")
+//baiso6
+//func printShortName(fullName: String) {
+//    let nameComponents = fullName.components(separatedBy: " ")
+//    if nameComponents.count >= 2 {
+//        let firstName = nameComponents.last!
+//        let lastName = nameComponents.first!
+//        print("\(lastName) \(firstName)")
+//    } else {
+//        print("Invalid name")
+//    }
+//}
+
